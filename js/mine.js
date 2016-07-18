7d57ac02d724b48981bd036aef85096fdf6fa014
wangze0608 <wangze0608@sina.com>
2016-07-18 22:09:27 +0800

    new page

---
+++ js/mine.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Õı‘Û on 2016/7/12.
+ */
+;(function(win,doc){
+    function change(){
+        doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/320+'px';
+    }
+    change();
+    win.addEventListener('resize',change,false);
+
+})(window,document);
---
+++ js/myswipter.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ÍõÔó on 2016/7/14.
+ */
+document.addEventListener('DOMContentloaded',function(){
+    alert(1);
+},false);
