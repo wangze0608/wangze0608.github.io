987e8f28472db4618d9bd44c9bc6971745c61d93
wangze0608 <wangze0608@sina.com>
2017-10-09 15:37:09 +0800

    vue test

---
+++ VUE2.0/js/myvue.js
@@ -0,0 +1,60 @@
+var vm = new Vue({
+	el: "#app",
+	data:{
+		static:'',
+		msgList:[],
+		page:0,
+		noMore:false,
+		scroll:'',
+	},
+	mounted:function () {
+		this.$nextTick(function () {
+			this.getIntDate();
+			var self = this;
+			$(window).scroll(function () {
+				let scrollTop = $(this).scrollTop();
+				let scrollHeight = $(document).height();
+				let windowHeight = $(this).height();
+				
+				if(scrollTop + windowHeight  === scrollHeight && self.noMore == false) {
+					self.getMoreDate();
+				}
+			})
+		});
+	},
+	watch:{
+		// page:function () {
+		// 	if( this.page == 4) {
+		// 		console.log(this.page);
+		// 		return this.noMore =true;
+		// 	}
+		// },
+	},
+	methods:{
+		getIntDate: function () {
+			let  _this = this;
+			this.$http.get('./data'+_this.page+'.json',{id:1,page:_this.page}).then(function (res) {
+				_this.msgList = res.body.data;
+				_this.page++;
+			});
+		},
+		getMoreDate:function () {
+			var _this = this;
+			if(!this.noMore ) {
+				this.$http.get('./data'+_this.page+'.json',{id:1,page:_this.page}).then(function (res) {
+					console.log(res.body.data.length);
+					if (res.body.data.length > 0 ) {
+						_this.msgList = _this.msgList.concat(res.body.data);
+						_this.page++;
+					} else {
+						this.noMore = true;
+					}
+					
+				});
+			} else {
+				console.log('没有更多数据了');
+			}
+			
+		},
+	},
+});
